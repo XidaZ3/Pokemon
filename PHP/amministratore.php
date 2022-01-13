a6ff49b1eebe2e96162bf728be4b6d23a51ff381
Xida <xidachen3@gmail.com>
2022-01-13 23:51:46 +0100

    Fix graphic upload
    Fix graphic upload

--- PHP/amministratore.php
+++ PHP/amministratore.php
@@ -4,10 +4,11 @@
     $paginaAmministratore = file_get_contents('../amministratore.html');
     if(isset($_SESSION['uploadError']) && is_array($_SESSION['uploadError'])){
         $error = $_SESSION['uploadError'];
-        $output ="";
+        $output ="<b>";
         foreach($error as $message){
-            $output = $output."<li><b>{$message}</b></li>";
+            $output = $output."{$message}<br />";
         }
+        $output = $output."</b>";
         $paginaAmministratore = str_replace('<output/>',$output,$paginaAmministratore);
     }
     echo $paginaAmministratore;
